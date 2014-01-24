0fbc14a9f08ffeb84f3c66ffe597aa5c9fbd4e4b
Matt Blair <elsewisemedia@gmail.com>
2014-01-24 15:05:55 -0800

    Added constants file

---
+++ EWAAudioPlayerViewDemo/DemoConstants.h
@@ -0,0 +1,14 @@
+//
+//  DemoConstants.h
+//  EWAAudioPlayerViewDemo
+//
+//  Created by Matt Blair on 1/24/14.
+//  Copyright (c) 2014 Elsewise. All rights reserved.
+//
+
+// Audio Player Images
+extern NSString* const kPlayButtonImage;
+extern NSString* const kPauseButtonImage;
+extern NSString* const kThumbButtonImage; // UISlider
+extern NSString* const kMinimumTrackImage; // UISlider
+extern NSString* const kMaximumTrackImage; // UISlider
---
+++ EWAAudioPlayerViewDemo/DemoConstants.m
@@ -0,0 +1,16 @@
+//
+//  DemoConstants.m
+//  EWAAudioPlayerViewDemo
+//
+//  Created by Matt Blair on 1/24/14.
+//  Copyright (c) 2014 Elsewise. All rights reserved.
+//
+
+#import "DemoConstants.h"
+
+// Audio Player Images
+NSString* const kPlayButtonImage = @"kbb-play";
+NSString* const kPauseButtonImage = @"kbb-pause";
+NSString* const kThumbButtonImage = @"kbb-audio-thumb";
+NSString* const kMinimumTrackImage = @"kbb-slider-2-filled"; // i.e. played
+NSString* const kMaximumTrackImage = @"kbb-slider-2-empty"; // i.e. yet to be played
